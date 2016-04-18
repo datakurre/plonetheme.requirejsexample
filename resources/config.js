e12d012d69c2cc9f12c6780a8888506941b6b6ed
Asko Soukka <asko.soukka@iki.fi>
2016-04-18 23:37:58 +0300

    Optimize build by removing license comments

--- resources/config.js
+++ resources/config.js
@@ -3,6 +3,7 @@
   name: "license-selector-pattern",
   out: "theme/example/license-selector-bundle.js",
   optimize: "uglify2",
+  preserveLicenseComments: false,
   paths: {
     "react": "../../node_modules/react/dist/react",
     "react-dom": "../../node_modules/react-dom/dist/react-dom",
